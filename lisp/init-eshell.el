20e5750307111b35f8c292e2f99a3cda9c56e6ad
Arseniy Tsipenyuk <arseniy.tsipenyuk@gmail.com>
2020-01-31 10:22:33 +0100

    add local folder to eshell PATH

--- lisp/init-eshell.el
+++ lisp/init-eshell.el
@@ -12,7 +12,11 @@
 ;;(add-hook 'emacs-startup-hook (lambda () (eshell) (previous-buffer)))
 (add-hook 'emacs-startup-hook 'eshell)
 ;;(cd (expand-file-name "~/.emacs.d"))
-(cd (expand-file-name "~/git/phd"))
+
+;; Fix eshell $PATH to include local installation of pdflatex
+(defun jpk/eshell-mode-hook ()
+  (eshell/addpath "/home/at/.texlive2019/bin/x86_64-linux"))
+(add-hook 'eshell-mode-hook #'jpk/eshell-mode-hook)
 
 (provide 'init-eshell)
 ;;; init-eshell.el ends here
