210483fbd030da03fd745fbbb6a5cdeb9a11e844
Arseniy Tsipenyuk <arseniy.tsipenyuk@gmail.com>
2020-04-12 23:26:23 +0200

    added \Cc-d to disable column-marker mode

--- lisp/init-column-marker.el
+++ lisp/init-column-marker.el
@@ -7,5 +7,11 @@
 (add-hook 'julia-mode-hook (lambda () (interactive) (column-marker-1 80)))
 (global-set-key [?\C-c ?m] 'column-marker-1)
 
+(global-set-key [?\C-c ?d]
+            (lambda ()
+              (interactive)
+              (let ((current-prefix-arg '(16))) ;; The result of C-u C-u.
+                (call-interactively 'column-marker-1))))
+
 (provide 'init-column-marker)
 ;;; init-column-marker.el ends here
